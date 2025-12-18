b8fa4cc87ecac7ba1ce7116dd77cb4939b3e9336
indrasvat <koffee2kode@gmail.com>
2025-12-18 00:21:04 -0800

    chore: update build info

--- Jools/Core/BuildInfo.swift
+++ Jools/Core/BuildInfo.swift
@@ -2,9 +2,9 @@
 import Foundation
 
 enum BuildInfo {
-    static let gitSHA = "18a54af"
+    static let gitSHA = "a8ff5ff"
     static let gitBranch = "create-jools"
-    static let buildDate = "2025-12-18 08:10 UTC"
+    static let buildDate = "2025-12-18 08:21 UTC"
     static let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0.0"
     static let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "1"
 
